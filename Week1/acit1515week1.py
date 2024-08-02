bcf1add1b0d78cda74b978a4b23b1d9db4fc0840
CH4HAT <156703119+CH4HAT@users.noreply.github.com>
2024-08-01 22:32:34 -0700

    Week1

---
+++ Week1/acit1515week1.py
@@ -0,0 +1,47 @@
+# single line comment 
+
+""" 
+    multi-line comment 
+
+"""
+
+"""
+    python is object-oriented language 
+    python is case--sensative c|=C
+
+    two ways we can run python (.py) files
+        scripts - self contained program/aplication
+        modules - file that is included in other python(.py) file  
+
+
+    Python relies on indentation for blocks of code    
+
+"""
+
+""" 
+Python operators
+    +     addition , joining strings together(concatnation)
+    -    substract
+    *    multiply
+    ==   equal
+    /    divide
+    =    assignment
+
+"""
+
+"""
+Data Types 
+
+    Strings - any sequence of characters inside of quotes
+    Boolean - True/false 
+    Numeric types -
+    to get datatype we use function type()  example is print(type(x)), it will return data type of varable x
+
+"""
+"""
+While adding string we can just put f inside of the brackets in th eprint function and enclose the variable in the epretheses and that should represent the assigned value and will return the values not the variable itself.
+    x=hello
+    y= hi
+    print(f'{x} is saying hello to {y}')
+
+"""
